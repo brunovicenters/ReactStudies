1ae20de5fdc9282820fb3b43879c54109f52816a
bruno <brunovicente.dev@gmail.com>
2024-05-28 17:17:00 -0300

    Added props to Medical Record

--- medical-record/src/App.tsx
+++ medical-record/src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
-import MedicalRecord from './components/MedicalRecord'
+import './App.css';
+import MedicalRecord from './components/MedicalRecord';
 
 function App() {
 
   return (
-    <main className="w-screen h-screen bg-slate-200 flex justify-center items-center">
-      <MedicalRecord />
+    <main className="w-screen h-screen bg-slate-600 flex justify-center items-center">
+      <MedicalRecord name='Vicente' age={19} gender='M' symptoms="Headache" />
     </main>
   )
 }
--- medical-record/src/components/MedicalRecord.tsx
+++ medical-record/src/components/MedicalRecord.tsx
@@ -1,3 +1,20 @@
-export default function MedicalRecord() {
-    return <div>MedicalRecord</div>
+export default function MedicalRecord(props: { name: string, age: number, gender: string, symptoms: string}) {
+    
+    return (
+        <div className="flex flex-col justify-center items-start">
+            <h1 className="text-3xl text-red-500">Medical Record</h1>
+            <p className="font-bold text-xl text-green-500">
+                Name: <span className="text-blue-500">{props.name}</span>
+            </p>
+            <p className="font-bold text-xl text-green-500">
+                Age: <span className="text-blue-500">{props.age}</span>
+            </p>
+            <p className="font-bold text-xl text-green-500">
+                Gender: <span className="text-blue-500">{props.gender}</span>
+            </p>
+            <p className="font-bold text-xl text-green-500">
+                Symptoms: <span className="text-blue-500">{props.symptoms}</span>
+            </p>
+        </div>
+    )
 }
