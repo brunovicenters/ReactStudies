8874c8fdc434a22e82c7b2c20d46c7ffcc457ab6
bruno <brunovicente.dev@gmail.com>
2024-05-28 17:43:54 -0300

    Added default values

--- medical-record/src/App.tsx
+++ medical-record/src/App.tsx
@@ -4,8 +4,12 @@ import MedicalRecord from './components/MedicalRecord';
 function App() {
 
   return (
-    <main className="w-screen h-screen bg-slate-600 flex justify-center items-center">
+    <main className="w-screen h-screen bg-slate-600 space-x-4 flex justify-center items-center">
       <MedicalRecord name='Vicente' age={19} gender='M' symptoms="Headache" />
+      <div className='w-1 h-24 bg-red-500'></div>
+      <MedicalRecord name='Tarsila' age={18} gender="F"  symptoms="Cough" />
+      <div className='w-1 h-24 bg-red-500'></div>
+      <MedicalRecord name='Alexis' age={15}  symptoms="Fever" />
     </main>
   )
 }
--- medical-record/src/components/MedicalRecord.tsx
+++ medical-record/src/components/MedicalRecord.tsx
@@ -1,19 +1,19 @@
-export default function MedicalRecord(props: { name: string, age: number, gender: string, symptoms: string}) {
+export default function MedicalRecord({name = 'Undefined', age, gender = 'Undefined', symptoms}) {
     
     return (
         <div className="flex flex-col justify-center items-start">
             <h1 className="text-3xl text-red-500">Medical Record</h1>
             <p className="font-bold text-xl text-green-500">
-                Name: <span className="text-blue-500">{props.name}</span>
+                Name: <span className="text-blue-500">{name}</span>
             </p>
             <p className="font-bold text-xl text-green-500">
-                Age: <span className="text-blue-500">{props.age}</span>
+                Age: <span className="text-blue-500">{age}</span>
             </p>
             <p className="font-bold text-xl text-green-500">
-                Gender: <span className="text-blue-500">{props.gender}</span>
+                Gender: <span className="text-blue-500">{gender}</span>
             </p>
             <p className="font-bold text-xl text-green-500">
-                Symptoms: <span className="text-blue-500">{props.symptoms}</span>
+                Symptoms: <span className="text-blue-500">{symptoms}</span>
             </p>
         </div>
     )
