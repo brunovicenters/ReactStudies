3601099c925a1d6fa6ddb4a9a629bed61263e0ec
bruno <brunovicente.dev@gmail.com>
2024-05-28 18:01:34 -0300

    Adding array to props

--- medical-record/src/components/MedicalRecord.tsx
+++ medical-record/src/components/MedicalRecord.tsx
@@ -1,7 +1,7 @@
-export default function MedicalRecord({name = 'Undefined', age, gender = 'Undefined', symptoms}) {
+export default function MedicalRecord({name = 'Anonymous', age, gender = 'Undefined', symptoms}: { name?: string, age: number, gender?: string, symptoms: string[] }) {
     
     return (
-        <div className="flex flex-col justify-center items-start">
+        <div className="flex flex-col justify-center items-start h-72 border-s-2 border-red-500 pl-2">
             <h1 className="text-3xl text-red-500">Medical Record</h1>
             <p className="font-bold text-xl text-green-500">
                 Name: <span className="text-blue-500">{name}</span>
@@ -13,7 +13,18 @@ export default function MedicalRecord({name = 'Undefined', age, gender = 'Undefi
                 Gender: <span className="text-blue-500">{gender}</span>
             </p>
             <p className="font-bold text-xl text-green-500">
-                Symptoms: <span className="text-blue-500">{symptoms}</span>
+                Symptoms:
+                <ul>
+                    {symptoms.map(s => {
+                        return (
+                        <li className="text-blue-500 flex items-center space-x-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                            </svg>
+                             {s}
+                        </li>)
+                    })}
+                </ul>
             </p>
         </div>
     )
