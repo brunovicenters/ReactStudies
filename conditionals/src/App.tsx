1bdaa5968dc7ec1960d305b7593bb16da583df37
bruno <brunovicente.dev@gmail.com>
2024-05-28 20:27:31 -0300

    Added  basic style

--- conditionals/src/App.tsx
+++ conditionals/src/App.tsx
@@ -4,9 +4,9 @@ import DoubleDice from './components/DoubleDice'
 function App() {
 
   return (
-    <>
+    <main className="h-screen flex items-center justify-center bg-gray-900">
       <DoubleDice />
-    </>
+    </main>
   )
 }
 
