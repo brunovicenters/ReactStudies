aede667989bbfa9ce8da4e7106b61e3a337c1915
bruno <brunovicente.dev@gmail.com>
2024-05-28 16:16:03 -0300

    Added initial setup

--- medical-record/src/App.tsx
+++ medical-record/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import MedicalRecord from './components/MedicalRecord'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main className="w-screen h-screen bg-slate-200 flex justify-center items-center">
+      <MedicalRecord />
+    </main>
   )
 }
 
