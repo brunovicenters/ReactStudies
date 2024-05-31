82df7a96ac0ac7b331725307544ece8240c18347
bruno <brunovicente.dev@gmail.com>
2024-05-31 15:36:43 -0300

    Made styles dynamic

--- conditionals/src/components/DoubleDice.tsx
+++ conditionals/src/components/DoubleDice.tsx
@@ -1,18 +1,20 @@
 export default function DoubleDice() {
     const rand1 = Math.floor(Math.random() * 3) + 1;
     const rand2 = Math.floor(Math.random() * 3) + 1;
+    const win = rand1 === rand2;
+    const styles = {backgroundColor: win ? 'green' : '#ff2e24'};
     
     return (
-        <div className="text-white">
+        <div className="text-white p-2 rounded-2xl" style={styles}>
             <h2 className="font-bold text-2xl">Double dice</h2>
             <ul>
                 <li>Dice 1: <span className="text-blue-300">{rand1}</span></li>
                 <li>Dice 2: <span className="text-blue-300">{rand2}</span></li>
                 <li>
-                    {rand1 === rand2 ? 'Doubles!' : 'Not doubles!'}
+                    {win ? 'Doubles!' : 'Not doubles!'}
                 </li>
             </ul>
-            {rand1 === rand2 && <h3 className="text-green-300">You win!</h3>}
+            {win && <h3 className="text-green-300">You win!</h3>}
         </div>
     )
 }
