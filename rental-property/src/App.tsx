632b93e06ae3402959c24808ad31cb242139d01e
bruno <brunovicente.dev@gmail.com>
2024-06-02 18:32:33 -0300

    First setup

---
+++ rental-property/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import PropertyList from './components/Property/PropertyList';
+
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+];
+
+function App() {
+
+  return (
+    <main  className='relative h-screen w-full flex justify-center items-center bg-slate-900'>
+      <img src="logo.svg" alt="My logo" className='absolute top-2 left-2' />
+      <PropertyList properties={properties} />
+    </main>
+  )
+}
+
+export default App
---
+++ rental-property/src/components/Property/PropertyList.tsx
@@ -0,0 +1,3 @@
+export default function PropertyList({ properties }: {properties: {id: number, name: string, rating: number, price: number}[]}) {
+    return <div>PropertyList</div>
+}
