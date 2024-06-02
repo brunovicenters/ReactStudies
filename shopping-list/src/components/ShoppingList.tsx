cfd67404faa71c5e112e6a0fa16c431a9896c13e
bruno <brunovicente.dev@gmail.com>
2024-06-02 10:24:52 -0300

    Added the spread operator

--- shopping-list/src/components/ShoppingList.tsx
+++ shopping-list/src/components/ShoppingList.tsx
@@ -7,7 +7,7 @@ export default function ShoppingList({data}: {data: {id: number, item: string, q
             <h1 className="text-3xl text-white mb-5">Shopping List</h1>
             <ul className="text-lg space-y-2">
                 {data.map((item: {id: number, item: string, quantity: number, completed: boolean}) => (
-                    <ShoppingItem key={item.id} item={item.item} quantity={item.quantity} completed={item.completed} />
+                    <ShoppingItem key={item.id} /*item={item.item} quantity={item.quantity} completed={item.completed}*/ {...item} />
                 ))}
             </ul>
         </div>
