87b99d2b89a8c5992a72d85076e82a7d6a090033
bruno <brunovicente.dev@gmail.com>
2024-06-02 22:21:58 -0300

    Added toggler content

--- toggler/src/components/Toggler.tsx
+++ toggler/src/components/Toggler.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
+
 export default function Toggler() {
+    const [isHappy, setIsHappy] = useState(false)
+    const toggleIsHappy = () => setIsHappy(!isHappy)
+
     return (
-        <div className="flex items-center justify-center">
-            <h1 className="text-3xl font-bold text-white uppercase">Toggler</h1>
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold text-white uppercase">{isHappy ? "😁" : "😟"}</h1>
+            <input onClick={toggleIsHappy} type="checkbox" className="mt-4 rounded-lg w-6 h-6" />
         </div>
     )
 }
