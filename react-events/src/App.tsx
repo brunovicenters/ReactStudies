65b047086dda4370e39e3a7c99658b4a8c60e2b3
bruno <brunovicente.dev@gmail.com>
2024-06-02 21:08:17 -0300

    Added clicker

--- react-events/src/App.tsx
+++ react-events/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Clicker from './components/Clicker'
 
 function App() {
 
   return (
     <main className="h-screen w-screen flex justify-center items-center relative bg-slate-900">
-
+      <Clicker />
       <img src="logo.svg" alt="My logo" className='absolute top-4 left-4 w-16' />
     </main>
   )
--- react-events/src/components/Clicker.tsx
+++ react-events/src/components/Clicker.tsx
@@ -1,5 +1,12 @@
+function handleClick() {
+    console.log('clicked')
+}
+
 export default function Clicker() {
     return (
-        <h1>Click the button</h1>
+        <div>
+            <h1 className="text-2xl text-white mb-4">Click the button</h1>
+            <button onClick={handleClick} className="hover:scale-105 w-full uppercase font-bold p-2 text-black bg-white/70 shadow-lg shadow-white/30 rounded-md">Click</button>
+        </div>
     )
 }
