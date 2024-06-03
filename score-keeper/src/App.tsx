113176b58f8de12dbeb85c571f78127bce50c4c8
bruno <brunovicente.dev@gmail.com>
2024-06-03 20:59:14 -0300

    First setup

---
+++ score-keeper/src/App.tsx
@@ -0,0 +1,12 @@
+import ScoreKeeper from "./components/ScoreKeeper"
+
+function App() {
+
+  return (
+    <main className="h-screen w-screen flex items-center justify-center bg-slate-900">
+      <ScoreKeeper />
+    </main>
+  )
+}
+
+export default App
---
+++ score-keeper/src/components/ScoreKeeper.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ScoreKeeper() {
+    return (
+        <div>
+            <h1>ScoreKeeper</h1>
+        </div>
+    )
+}
