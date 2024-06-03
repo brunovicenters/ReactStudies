2364da90fb8b9bdf5da22f293c52d664bb4ba32c
bruno <brunovicente.dev@gmail.com>
2024-06-02 22:02:27 -0300

    First setup

---
+++ toggler/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Toggler from './components/Toggler'
+
+function App() {
+
+  return (
+    <main className="h-screen w-screen flex items-center justify-center bg-slate-900">
+      <Toggler />
+    </main>
+  )
+}
+
+export default App
---
+++ toggler/src/components/Toggler.tsx
@@ -0,0 +1,7 @@
+export default function Toggler() {
+    return (
+        <div className="flex items-center justify-center">
+            <h1 className="text-3xl font-bold text-white uppercase">Toggler</h1>
+        </div>
+    )
+}
