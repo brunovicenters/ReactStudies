711f3da378fc0428f8ec87610cda6ad8f95ea635
bruno <brunovicente.dev@gmail.com>
2024-06-02 21:24:58 -0300

    Changed to h2

--- react-events/src/components/Clicker.tsx
+++ react-events/src/components/Clicker.tsx
@@ -5,7 +5,7 @@ function handleClick() {
 export default function Clicker() {
     return (
         <div>
-            <h1 className="text-2xl text-white mb-4">Click the button</h1>
+            <h2 className="text-2xl text-white mb-4">Click the button</h2>
             <button onClick={handleClick} className="hover:scale-105 w-full uppercase font-bold p-2 text-black bg-white/70 shadow-lg shadow-white/30 rounded-md">Click</button>
         </div>
     )
