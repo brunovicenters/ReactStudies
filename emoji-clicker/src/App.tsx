7ba499aecfa1fd448d0b86e92dd44a585373ea45
bruno <brunovicente.dev@gmail.com>
2024-06-03 21:34:14 -0300

    Added emoji clicker basic logic

--- emoji-clicker/src/App.tsx
+++ emoji-clicker/src/App.tsx
@@ -1,7 +1,9 @@
+import EmojiClicker from "./components/EmojiClicker"
+
 function App() {
   return (
     <main className="h-screen w-screen flex items-center justify-center bg-slate-900">
-      
+      <EmojiClicker />
     </main>
   )
 }
---
+++ emoji-clicker/src/components/EmojiClicker.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const emojiCollection: string[] = ['🤠', '🥳', '🥸', '😎'];
+
+export default function EmojiClicker() {
+    const [emojis, setEmojis] = useState(['🤠']);
+
+    const addEmoji = () => {
+        const newEmoji = emojiCollection[Math.floor(Math.random()*emojiCollection.length)]
+
+        setEmojis(()=> [...emojis, newEmoji])
+    }
+
+    return (
+        <div className="flex flex-col items-center space-y-4">
+            <button onClick={addEmoji} className="shadow-lg bg-green-300 shadow-green-300 hover:bg-green-400 hover:shadow-green-400 text-slate-600 font-bold py-2 px-4 rounded-lg">Add emoji</button>
+            <div className="flex flex-wrap gap-2 text-white text-3xl">{emojis.map((emoji, index) => <span key={index}>{emoji}</span>)}</div>
+        </div>
+    )
+}
