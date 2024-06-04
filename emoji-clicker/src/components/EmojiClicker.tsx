d9fb56cc3b40ed38c1572b52fbc5449c175baae3
bruno <brunovicente.dev@gmail.com>
2024-06-04 11:37:14 -0300

    Added all heart

--- emoji-clicker/src/components/EmojiClicker.tsx
+++ emoji-clicker/src/components/EmojiClicker.tsx
@@ -9,18 +9,27 @@ export default function EmojiClicker() {
     const addEmoji = () => {
         const randEmoji = emojiCollection[Math.floor(Math.random()*emojiCollection.length)]
         const newEmoji = {id: uuid(), emoji: randEmoji}
-        setEmojis(()=> [...emojis, newEmoji])
+        setEmojis((prevEmojis)=> [...prevEmojis, newEmoji])
     }
 
     const deleteEmoji = (id: string) => {
-        setEmojis((prevEmoji) => {
-            return prevEmoji.filter((emoji: {id: string, emoji: string}) => emoji.id !== id)
+        setEmojis((prevEmojis) => {
+            return prevEmojis.filter((emoji: {id: string, emoji: string}) => emoji.id !== id)
+        })
+    }
+
+    const makeAllHeart = () => {
+        setEmojis((prevEmojis) => {
+            return prevEmojis.map((e: {id: string, emoji: string}) => {
+                return {...e, emoji: '❤️'}
+            })
         })
     }
 
     return (
         <div className="flex flex-col items-center space-y-4">
             <button onClick={addEmoji} className="shadow-lg bg-green-300 shadow-green-300 hover:bg-green-400 hover:shadow-green-400 text-slate-600 font-bold py-2 px-4 rounded-lg">Add emoji</button>
+            <button onClick={makeAllHeart} className="shadow-lg bg-red-300 shadow-red-300 hover:text-white hover:bg-red-400 hover:shadow-red-400 text-slate-600 font-bold py-2 px-4 rounded-lg">All heart</button>
             <div className="flex flex-wrap gap-2 text-white text-3xl">
                 {emojis.map((emoji: {id: string, emoji: string}) => {
                     return (
